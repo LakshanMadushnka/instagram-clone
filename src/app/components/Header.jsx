cd3f0176eeaadedcd1b017e781347726d1120098
LakshanMadushnka <lranasinghe286@gmail.com>
2024-12-08 15:26:54 +0530

    add modal functionality using react modal

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,12 +2,16 @@
 
 import Link from 'next/link'
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
 import { signIn, useSession, signOut } from 'next-auth/react';
+import Modal from 'react-modal'
+import { IoAddCircleOutline } from "react-icons/io5";
+import { FaCamera } from "react-icons/fa";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function Header() {
   const {data: session} = useSession ();
-  console.log(session);
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
      <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -30,7 +34,11 @@ export default function Header() {
         {/* menu items */}
 
         {session ? (
+            <div className='flex gap-2 items-center'>
+            <IoAddCircleOutline className='text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600' onClick={() => setIsOpen(true)}/>
+            
             <img src={session.user.image} alt={session.user.name} className='h-10 w-10 rounded-full cursor-pointer' onClick = {signOut}/>
+            </div>
         ) : (
           <button onClick={signIn} className='text-sm font-semibold text-blue-500'> Log In </button>
         )}
@@ -38,7 +46,26 @@ export default function Header() {
 
      </div>
 
-
+        {
+          isOpen && (
+            <Modal isOpen={isOpen} className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+              onRequestClose={() => setIsOpen(false)}
+              ariaHideApp = {false}
+            >
+              <div className='flex felx-col justify-center items-center h-[100%]'>
+              <FaCamera className='text-5xl text-gray-400 cursor-pointer'/>
+              </div>
+              <input 
+                type='text'
+                maxLength='150'
+                placeholder='Please enter your caption'
+                className='m-4 border-none text-center w-full focus:ring-0 outline-none'
+              />
+              <button disabled className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105  disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'>Upload Post</button>
+              <IoCloseOutline className='cursor-pointer absolute top-2 right-2 hover:text-red-600 duration-300' onClick={() => setIsOpen(false)}/>
+            </Modal>
+          )}
+        
 
 
 
